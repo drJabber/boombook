6055135acef8eb8e2de2cd2e725c6e728f7433b3
dJabber@gmail.com <dJabber@gmail.com>
2019-02-11 13:20:00 +0300

    staff services

--- code/src/main/java/rnk/bb/services/StaffService.java
+++ code/src/main/java/rnk/bb/services/StaffService.java
@@ -19,5 +19,66 @@ public class StaffService {
         return this.staff.findByLogin(login);
     }
 
+    public StaffUserBean initStaffBean(StaffUserBean staffBean, Long staffId){
+        Staff staff=null;
+        if (staffId!=null){
+            staff=this.staff.findOptionalById(staffId).orElse(null);
+        }
+        StaffUserBean result=staffBean;
+        if (staffBean==null)
+        {
+            result=new StaffUserBean();
+        }
+        return initFoodConceptBean(result,staff);
+    }
+
+
+    private EditFoodConceptBean cleanStaffBean(StaffUserBean staffBean){
+        staffBean.setId(null);
+        staffBean.setLogin(null);
+        staffBean.setHotel(null);
+        staffBean.setName("");
+        staffBean.setBirthDate(null);
+        staffBean.setGender("М");
+        staffBean.setApproved(false);
+
+        return fcBean;
+    }
+
+    public StaffUserBean initStaffBean(StaffUserBean staffBean, Staff staff){
+        if (staff!=null) {
+            staffBean.setId(staff.getId());
+            staffBean.setLogin(staff.getLogin());
+            staffBean.setHotel(staff.getHotel());
+            staffBean.setName(staff.getName());
+            staffBean.setBirthDate(staff.getBirthDate());
+            staffBean.setGender(staff.getGender());
+            staffBean.setApproved(false);
+
+            return staffBean;
+        }else{
+            return cleanStaffBean(staffBean);
+        }
+    }
+
+    public StaffUserBean initStaffBean(StaffUserBean staffBean, StaffUserBean anotherBean){
+        if ((staffBean!=null)&&(anotherBean!=null)) {
+            Staff staff=this.staff.findByLongId(anotherBean.getId());
+
+            staffBean.setId(staff.getId());
+            staffBean.setLogin(staff.getLogin());
+            staffBean.setHotel(staff.getHotel());
+            staffBean.setName(staff.getName());
+            staffBean.setBirthDate(staff.getBirthDate());
+            staffBean.setGender(staff.getGender());
+            staffBean.setApproved(false);
+
+            return staffBean;
+        }else{
+            return cleanStaffBean(staffBean);
+        }
+    }
+
+    
 
 }
